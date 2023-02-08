7ded09813f9775c446de9c722d0c39d8e3bb0c65
M83 <75308710+M83XD@users.noreply.github.com>
2023-02-08 23:50:23 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+<script>
+   function showMessage() {
+      var name = prompt("İsmin Nedir?");
+      alert("Merhaba, " + name + "!");
+   }
+</script>
+
+<button onclick="showMessage()">Tıkla!</button>
